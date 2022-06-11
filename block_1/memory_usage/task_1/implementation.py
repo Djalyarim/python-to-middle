141a6917565bca44e2a0a815746a486c9ea7cdcc
Кузьмин Максим <m.kuzmin@bars.group>
2022-06-11 16:29:48 +0500

    first_task_done

--- block_1/memory_usage/task_1/implementation.py
+++ block_1/memory_usage/task_1/implementation.py
@@ -1,5 +1,15 @@
+import os
+from contextlib import contextmanager
+
+
+@contextmanager
 class TempFile:
 
     def __init__(self, path) -> None:
         super().__init__()
+        self.path = 'file.tmp'
         self._f = open(path, 'wb')
+
+    def __del__(self):
+        self._f.close()
+        os.remove(self.path)
