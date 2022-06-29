f192543895b9d69efeee43128f16c071a7b557fc
Кузьмин Максим <m.kuzmin@bars.group>
2022-06-29 11:33:10 +0500

    test

--- block_1/memory_usage/task_2/implementation.py
+++ block_1/memory_usage/task_2/implementation.py
@@ -1,24 +1,44 @@
+import gc
+import sys
+
 class MyObject:
+    instance = 0
 
     def __init__(self, name) -> None:
         super().__init__()
         self.name = name
+        MyObject.instance += 1
+        self.count = MyObject.instance
 
 
 def cache(func):
-    memo = {}
+    """
+    Обертка, кэширующая результат.
+    """
+    cached = {}
+    func._cache = cached
 
-    def wrapper(*args):
-        if args in memo:
-            return memo[args]
+    def wrapped(*args):
+        nonlocal cached
+        if not cached.get(args):
+            result = func(*args)
+            cached[args] = result
+            return result
         else:
-            rv = func(*args)
-            memo[args] = rv
-            return rv
-
-    return wrapper
+            return cached[args]
+    return wrapped
 
 
 @cache
 def create_object(name):
+    create_object._cache = name
     return MyObject(name)
+
+
+print(MyObject.instance)
+aaa = create_object('A')
+print(MyObject.instance)
+create_object('B')
+print(MyObject.instance)
+
+print(dir(create_object))
